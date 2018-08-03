3e0c60b5b445fd21e96d945e2190f2a570cfced1
i-obr <obr.iv@ya.ru>
2018-08-03 10:46:17 +0300

    fix url

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const PATH_BASE = 'http://aviation-edge.com/api/public';
+export const PATH_BASE = 'https://aviation-edge.com/api/public';
 export const PATH_SEARCH = '/timetable';
 export const USER_KEY = '72070f-f2db7f-b6315f-26a9a4-52ee52';
 export const ITA_CODE = 'SVO';
